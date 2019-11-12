13e9bf2fd279895eecb7d8c8bd878c212df7f5b7
Vincent Poulain <vincent.poulain2@gmail.com>
2019-11-12 16:13:03 +0100

    Use graphqlPath to /graphql

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import { ApolloServer } from 'apollo-server'
-import { createContext } from './context'
-import { schema } from './schema'
-import dotenv from 'dotenv'
+import { ApolloServer } from "apollo-server";
+import { createContext } from "./context";
+import { schema } from "./schema";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const server = new ApolloServer({
-    schema,
-    context: createContext(),
-    engine: {
-        apiKey: process.env.ENGINE_API_KEY
-    }
+  schema,
+  context: createContext(),
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY
+  }
 });
-server.listen({port: 4000}, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`))
+
+server.listen({ port: 4000, graphqlPath: "/graphql" }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`)
+);
