d111c62fb9c40f968ae40f7d574079ff8072e35f
Vincent Poulain <vincent.poulain2@gmail.com>
2019-11-13 17:40:57 +0100

    Add Winston transport to console on production

--- src/logging.ts
+++ src/logging.ts
@@ -14,12 +14,12 @@ const logger = createLogger({
     defaultMeta: { service: 'tottem-api' },
 })
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-        new transports.Console({
-            format: format.combine(format.colorize(), format.simple()),
-        })
-    )
-}
+// if (process.env.NODE_ENV !== 'production') {
+logger.add(
+    new transports.Console({
+        format: format.combine(format.colorize(), format.simple()),
+    })
+)
+// }
 
 export default logger
